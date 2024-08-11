6a1134135c80d01b91d7e3d075443a2a388bef98
Esaivani2003 <146462704+Esaivani2003@users.noreply.github.com>
2024-08-11 21:58:48 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,13 @@
+ $(document).ready(function(){
+        $('.carousel').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        arrows: true,
+        dots: true,
+        fade: false,
+        cssEase: 'linear'
+    });
+});
